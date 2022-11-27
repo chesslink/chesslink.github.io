f0a6dbc5f42fffee9e52b8ed7977a0bcfc1a0ca5
David Revelj <david@revelj.se>
2022-11-27 19:39:16 +0100

    another google analytics attempt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const measurementId = "G-0THG02423M";
+  const GA_TRACKING_ID = "G-0THG02423M";
 
   return (
     <>
@@ -23,28 +22,25 @@ export default function App({ Component, pageProps }: AppProps) {
           property="og:image"
           content="https://chesslink.github.io/zoe-holling-UDfmSK4AS3E-unsplash-crop.jpg"
         />
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        ></script>
+        <script
+          type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('consent', 'default', {
-            'ad_storage': 'denied',
-            'analytics_storage': 'denied'
-          });
-          gtag('config', '${measurementId}', {
-            page_path: window.location.pathname,
-          });
-        `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'analytics_storage': 'denied'
+              });
+              gtag('config', '${GA_TRACKING_ID}');
+              `,
           }}
-        />
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
