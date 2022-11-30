1fbb465c02b96c9e21ad4a14dd93f3b5b74cc46e
David Revelj <david@revelj.se>
2022-11-30 22:36:32 +0100

    build

--- docs/_next/static/CcDGNEyiOLHbl9cKkLBNI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/626-16703598eb948a2d.js","static/chunks/pages/index-377cc3a8281a0b33.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
